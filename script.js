0aa98d869313218223e8487be49f64e1493e9618
Gopalk1408 <115712807+Gopalk1408@users.noreply.github.com>
2022-10-18 10:36:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -72,6 +72,119 @@ function loadPlaces(position) {
                 lng: 76.97016781047577
             },
         },
+        {
+            name: 'DJAME',  
+            location: {
+                        lat:  10.87211910088659, 
+                        lng: 77.00329432446298
+            }
+        },
+        {
+            name: 'Workshop',  
+            location: {
+                        lat: 10.871907854292578, 
+                        lng:77.00252986528507
+            }
+        },
+        {
+            name: 'Basketball',  
+            location: {
+                        lat: 10.871854439545329, 
+                        lng: 77.00168206434873
+            }
+        },
+        {
+            name: 'DJAD',  
+            location: {
+                        lat: 10.872391878067093, 
+                        lng: 77.00233236699457
+            }
+        },
+        {
+            name: 'Waav',  
+            location: {
+                        lat: 10.872371922623035, 
+                        lng: 77.0020004704595
+            }
+        },
+        {
+            name: 'Library',  
+            location: {
+                        lat: 10.87289575260213, 
+                        lng: 77.00264055663018
+            }
+        },
+        {
+            name: 'Dabba',  
+            location: {
+                        lat: 10.873417259869253, 
+                        lng: 77.00220372728621
+            }
+        },
+        {
+            name: 'Cafeteria',  
+            location: {
+                        lat: 10.872903904118381, 
+                        lng: 77.00173189044992
+            }
+        },
+        {
+            name: 'Foundees Block',  
+            location: {
+                        lat: 10.872278208340134, 
+                        lng: 77.00156438123518
+            }
+        },
+        {
+            name: 'Design Block',  
+            location: {
+                        lat: 10.872517673660923, 
+                        lng: 77.00201311890666
+            }
+        },
+        {
+            name: 'Mens Hostel',  
+            location: {
+                        lat: 10.873499912026068, 
+                        lng: 77.0016485362464
+            }
+        },
+        {
+            name: 'Womens Hostel',  
+            location: {
+                        lat: 10.874076987995402, 
+                        lng: 77.00219478080179
+            }
+        },
+        {
+            name: 'Parking',  
+            location: {
+                        lat: 10.873578299042608, 
+                        lng: 77.00379181742726
+            }
+        },
+        {
+            name: 'Temple',  
+            location: {
+                        lat: 10.873987880822291, 
+                        lng: 77.00367364412776
+            }
+        },
+        {
+            name: 'Guest House?',  
+            location: {
+                        lat: 10.874347926910907, 
+                        lng: 77.00410158689384
+            }
+        },
+        {
+            name: 'Main Gate',  
+            location: {
+                        lat: 10.875595576759162, 
+                        lng: 77.00453924217922
+            }
+        }
+        
     ];
 };
 
@@ -87,7 +200,7 @@ window.onload = () => {
                 const latitude = place.location.lat;
                 const longitude = place.location.lng;
 
-                alert(place.name);
+                //alert(place.name);
 
                 // add place name
                 const placeText = document.createElement('a-link');
