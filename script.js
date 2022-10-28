5ac0a105a79c97c593f4c3b76a7df7c16e96af93
Gopalk1408 <115712807+Gopalk1408@users.noreply.github.com>
2022-10-28 15:50:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -176,8 +176,6 @@ window.onload = () => {
                 const placeText = document.createElement('a-link');
                 placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                 placeText.setAttribute('title', place.name);
-                placeText.setAttribute('href', 'http://www.djad.in/');
-                placeText.setAttribute('src', './images/map-marker.png');
                 placeText.setAttribute('scale', '3 3 3');
                 
                 placeText.addEventListener('loaded', () => {
